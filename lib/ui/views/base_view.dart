ac0e1df3ff274e4b84df4855671d71b64d057ec7
Nitish Aggarwal <royalnitish21@gmail.com>
2020-05-07 20:35:57 +0530

    Set Up the basic folder structure, views, viewmodels and services (#1)
    * Updates .gitignore
    
    * Creates flutter project
    
    * Add basic project structure and services.
    
    - Add dialog service.
    - Add navigation service.
    - Add local storage service using shared_prefs
    - Add base viewmodel and views.
    - Wire up startup, login, and home viewmodel and view.
    
    * conf: Add .github workflow
    
    * conf: Update to flutter 1.18
    
    * test: Add startup view tests
    
    * conf: Add /coverage to .gitignore

---
+++ lib/ui/views/base_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/locator.dart';
+import 'package:mobile_app/viewmodels/base_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+class BaseView<T extends BaseModel> extends StatefulWidget {
+  final Widget Function(BuildContext context, T model, Widget child) builder;
+  final Function(T) onModelReady;
+  final Function(T) onModelDestroy;
+
+  BaseView({this.builder, this.onModelReady, this.onModelDestroy});
+
+  @override
+  _BaseViewState<T> createState() => _BaseViewState<T>();
+}
+
+class _BaseViewState<T extends BaseModel> extends State<BaseView<T>> {
+  T model = locator<T>();
+
+  @override
+  void initState() {
+    if (widget.onModelReady != null) {
+      widget.onModelReady(model);
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (widget.onModelDestroy != null) {
+      widget.onModelDestroy(model);
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>.value(
+      value: model,
+      child: Consumer<T>(
+        builder: widget.builder,
+      ),
+    );
+  }
+}
