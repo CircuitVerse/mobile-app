fd43a9d627ec1fdb09b0ed1645acf79868b78fdf
Nitish Aggarwal <royalnitish21@gmail.com>
2020-07-18 08:44:06 +0530

    ptch: Add Nav drawer to home_view

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,19 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:get/get.dart';
 import 'package:mobile_app/ui/components/cv_outline_button.dart';
 import 'package:mobile_app/ui/components/cv_subheader.dart';
+import 'package:mobile_app/ui/views/about/about_view.dart';
+import 'package:mobile_app/ui/views/authentication/login_view.dart';
 import 'package:mobile_app/ui/views/base_view.dart';
-import 'package:mobile_app/ui/views/home/feature_card.dart';
-import 'package:mobile_app/viewmodels/home_viewmodel.dart';
+import 'package:mobile_app/ui/views/contributors/contributors_view.dart';
+import 'package:mobile_app/ui/views/groups/my_groups_view.dart';
+import 'package:mobile_app/ui/views/home/components/feature_card.dart';
+import 'package:mobile_app/ui/views/profile/profile_view.dart';
+import 'package:mobile_app/ui/views/projects/favourite_projects_view.dart';
+import 'package:mobile_app/ui/views/projects/my_projects_view.dart';
+import 'package:mobile_app/ui/views/teachers/teachers_view.dart';
+import 'package:mobile_app/viewmodels/home/home_viewmodel.dart';
 
-class HomeView extends StatelessWidget {
-  static const String id = "home_view";
+class HomeView extends StatefulWidget {
+  static const String id = 'home_view';
 
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     Widget _buildAppBar() {
       return AppBar(
-        title: Text("CircuitVerse"),
+        title: Text('CircuitVerse'),
         centerTitle: true,
+      );
+    }
+
+    Widget _buildDrawerTile(String title, IconData iconData) {
+      return ListTile(
+        leading: Icon(iconData),
+        title: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      );
+    }
+
+    Widget _buildDrawer(HomeViewModel model) {
+      return Drawer(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 32),
+              child: Text(
+                'CircuitVerse',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline3.copyWith(
+                      fontFamily: 'Grinched',
+                      color: Theme.of(context).primaryColor,
+                    ),
+              ),
+            ),
+            InkWell(
+              child: _buildDrawerTile('About', FontAwesome5.address_card),
+              onTap: () => Get.offAndToNamed(AboutView.id),
+            ),
+            InkWell(
+              child: _buildDrawerTile('Contribute', Icons.add),
+              onTap: () => Get.offAndToNamed(ContributorsView.id),
+            ),
+            InkWell(
+              child: _buildDrawerTile('Teachers', Icons.account_balance),
+              onTap: () => Get.offAndToNamed(TeachersView.id),
+            ),
+            model.isLoggedIn
+                ? ExpansionTile(
+                    title: Text(
+                      model.currentUserName,
+                      style: Theme.of(context).textTheme.headline6.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    children: <Widget>[
+                      InkWell(
+                        child: _buildDrawerTile('Profile', FontAwesome5.user),
+                        onTap: () => Get.offAndToNamed(ProfileView.id),
+                      ),
+                      InkWell(
+                        child: _buildDrawerTile(
+                            'My Projects', FontAwesome5.address_book),
+                        onTap: () => Get.offAndToNamed(MyProjectsView.id),
+                      ),
+                      InkWell(
+                        child: _buildDrawerTile(
+                            'My Groups', FontAwesome5.object_group),
+                        onTap: () => Get.offAndToNamed(MyGroupsView.id),
+                      ),
+                      InkWell(
+                        child: _buildDrawerTile(
+                            'My Favourites', Icons.bookmark_border),
+                        onTap: () =>
+                            Get.offAndToNamed(FavouriteProjectsView.id),
+                      ),
+                      InkWell(
+                        child:
+                            _buildDrawerTile('Log Out', Ionicons.ios_log_out),
+                        onTap: () {
+                          Get.back();
+                          model.onLogout();
+                        },
+                      ),
+                    ],
+                  )
+                : InkWell(
+                    child: _buildDrawerTile('Login', Ionicons.ios_log_in),
+                    onTap: () => Get.offAndToNamed(LoginView.id),
+                  )
+          ],
+        ),
       );
     }
 
@@ -21,14 +121,14 @@ class HomeView extends StatelessWidget {
       return Column(
         children: <Widget>[
           Text(
-            "Dive into the world of Logic Circuits for free!",
+            'Dive into the world of Logic Circuits for free!',
             style: Theme.of(context).textTheme.headline4.copyWith(
                   fontWeight: FontWeight.bold,
                 ),
             textAlign: TextAlign.center,
           ),
           Text(
-            "From Simple gates to complex sequential circuits, plot timimg diagrams, automatic circuit generation, explore standard ICs, and much more",
+            'From Simple gates to complex sequential circuits, plot timimg diagrams, automatic circuit generation, explore standard ICs, and much more',
             style: Theme.of(context).textTheme.subtitle1,
             textAlign: TextAlign.center,
           ),
@@ -38,7 +138,7 @@ class HomeView extends StatelessWidget {
 
     Widget _buildHomePageSketch() {
       return Image.asset(
-        "assets/images/homepage/new-homepage-sketch.png",
+        'assets/images/homepage/new-homepage-sketch.png',
         gaplessPlayback: true,
       );
     }
@@ -49,9 +149,17 @@ class HomeView extends StatelessWidget {
         child: Wrap(
           alignment: WrapAlignment.center,
           children: <Widget>[
-            CVOutlineButton(title: "For Teachers"),
+            CVOutlineButton(
+              title: 'For Teachers',
+              onPressed: () => Get.toNamed(TeachersView.id),
+              isBodyText: true,
+            ),
             SizedBox(width: 16),
-            CVOutlineButton(title: "For Contributors"),
+            CVOutlineButton(
+              title: 'For Contributors',
+              onPressed: () => Get.toNamed(ContributorsView.id),
+              isBodyText: true,
+            ),
           ],
         ),
       );
@@ -60,6 +168,7 @@ class HomeView extends StatelessWidget {
     return BaseView<HomeViewModel>(
       builder: (context, model, child) => Scaffold(
         appBar: _buildAppBar(),
+        drawer: _buildDrawer(model),
         body: SingleChildScrollView(
           padding: const EdgeInsets.all(16),
           child: Column(
@@ -68,39 +177,39 @@ class HomeView extends StatelessWidget {
               _buildHomePageSketch(),
               _buildTeachersAndContributorButtons(),
               CVSubheader(
-                title: "Features",
+                title: 'Features',
                 subtitle:
-                    "Design circuits quickly and easily with a modern and intuitive user interface with drag-and-drop, copy/paste, zoom and more.",
+                    'Design circuits quickly and easily with a modern and intuitive user interface with drag-and-drop, copy/paste, zoom and more.',
               ),
               FeatureCard(
-                assetPath: "assets/images/homepage/export-hd.png",
-                cardHeading: "Explore High Resolution Images",
+                assetPath: 'assets/images/homepage/export-hd.png',
+                cardHeading: 'Explore High Resolution Images',
                 cardDescription:
-                    "CircuitVerse can export high resolution images in multiple formats including SVG.",
+                    'CircuitVerse can export high resolution images in multiple formats including SVG.',
               ),
               FeatureCard(
-                assetPath: "assets/images/homepage/combinational-analysis.png",
-                cardHeading: "Combinational Analysis",
+                assetPath: 'assets/images/homepage/combinational-analysis.png',
+                cardHeading: 'Combinational Analysis',
                 cardDescription:
-                    "Automatically generate circuit based on truth table data. This is great to create complex logic circuits and can be easily be made into a subcircuit.",
+                    'Automatically generate circuit based on truth table data. This is great to create complex logic circuits and can be easily be made into a subcircuit.',
               ),
               FeatureCard(
-                assetPath: "assets/images/homepage/embed.png",
-                cardHeading: "Embed in Blogs",
+                assetPath: 'assets/images/homepage/embed.png',
+                cardHeading: 'Embed in Blogs',
                 cardDescription:
-                    "Since CircuitVerse is built in HTML5, an iFrame can be generated for each project allowing the user to embed it almost anywhere.",
+                    'Since CircuitVerse is built in HTML5, an iFrame can be generated for each project allowing the user to embed it almost anywhere.',
               ),
               FeatureCard(
-                assetPath: "assets/images/homepage/sub-circuit.png",
-                cardHeading: "Use Sub circuits",
+                assetPath: 'assets/images/homepage/sub-circuit.png',
+                cardHeading: 'Use Sub circuits',
                 cardDescription:
-                    "Create subcircuits once and use them repeatedly. This allows easier and more structured design.",
+                    'Create subcircuits once and use them repeatedly. This allows easier and more structured design.',
               ),
               FeatureCard(
-                assetPath: "assets/images/homepage/multi-bit-bus.png",
-                cardHeading: "Multi Bit Buses and components",
+                assetPath: 'assets/images/homepage/multi-bit-bus.png',
+                cardHeading: 'Multi Bit Buses and components',
                 cardDescription:
-                    "CircuitVerse supports multi bit wires, this means circuit design is easier, faster and uncluttered.",
+                    'CircuitVerse supports multi bit wires, this means circuit design is easier, faster and uncluttered.',
               ),
             ],
           ),
