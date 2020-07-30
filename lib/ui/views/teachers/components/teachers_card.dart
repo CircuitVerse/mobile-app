f855e9e6dd0a8b624f863ed4d27efa74c4496d5e
Nitish Aggarwal <royalnitish21@gmail.com>
2020-07-30 11:16:26 +0530

    Add Teachers View & related tests (#6)
    * feat: Add teachers view & components
    
    * test: Add teachers view tests
    
    * ptch: minor UI improvements

---
+++ lib/ui/views/teachers/components/teachers_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class TeachersCard extends StatelessWidget {
+  final String assetPath;
+  final String cardHeading;
+  final String cardDescription;
+
+  const TeachersCard({
+    Key key,
+    this.assetPath,
+    this.cardDescription,
+    this.cardHeading,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 16),
+      elevation: 5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              assetPath,
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              cardHeading,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              cardDescription,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
