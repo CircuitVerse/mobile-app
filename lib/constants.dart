3dff08e1ff065aee76acab66fddee8406424be80
Nitish Aggarwal <royalnitish21@gmail.com>
2020-07-31 16:01:55 +0530

    Add About View & related tests (#5)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,12 @@
-
+class Constants {
+  /// GENERIC FAILURE CONSTANTS
+  static const String BAD_RESPONSE_FORMAT = 'Bad Response Format';
+  static const String INVALID_PARAMETERS = 'Invalid parameters, Retry!';
+  static const String GENERIC_FAILURE =
+      'Something Wrong Occurred! Please try again.';
+  static const String NO_INTERNET_CONNECTION = 'No Internet Connection';
+  static const String HTTP_EXCEPTION = 'Unable to fetch response';
+  static const String UNAUTHENTICATED =
+      'Unauthenticated to perform this action';
+  static const String UNAUTHORIZED = 'Unauthorized to perform this action';
+}
