0c7b360d163c732386fe394fd9b514de8fb02cd7
Nitish Aggarwal <royalnitish21@gmail.com>
2020-08-23 13:05:04 +0530

    test: Add groups & assignments service tests

---
+++ test/service_tests/grades_api_test.dart
@@ -0,0 +1,108 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'package:mobile_app/models/failure_model.dart';
+import 'package:mobile_app/models/grade.dart';
+import 'package:mobile_app/services/API/grades_api.dart';
+import 'package:mobile_app/utils/api_utils.dart';
+import 'package:mobile_app/utils/app_exceptions.dart';
+
+import '../setup/test_data/mock_grade.dart';
+import '../setup/test_helpers.dart';
+
+void main() {
+  group('GradesApiTest -', () {
+    setUpAll(() => registerServices());
+    tearDownAll(() => unregisterServices());
+
+    var _grade = Grade.fromJson(mockGrade['data']);
+
+    group('addGrade -', () {
+      test('When called & http client returns success response', () async {
+        ApiUtils.client = MockClient(
+            (_) => Future.value(Response(jsonEncode(mockGrade), 201)));
+        var _gradesApi = HttpGradesApi();
+
+        expect((await _gradesApi.addGrade('1', '1', 'A', 'Good')).toString(),
+            _grade.toString());
+      });
+
+      test('When called & http client throws Exceptions', () async {
+        var _gradesApi = HttpGradesApi();
+
+        ApiUtils.client = MockClient((_) => throw ForbiddenException(''));
+        expect(_gradesApi.addGrade('1', '1', 'A', 'Good'),
+            throwsA(isInstanceOf<Failure>()));
+
+        ApiUtils.client = MockClient((_) => throw NotFoundException(''));
+        expect(_gradesApi.addGrade('1', '1', 'A', 'Good'),
+            throwsA(isInstanceOf<Failure>()));
+
+        ApiUtils.client =
+            MockClient((_) => throw UnprocessableIdentityException(''));
+        expect(_gradesApi.addGrade('1', '1', 'A', 'Good'),
+            throwsA(isInstanceOf<Failure>()));
+
+        ApiUtils.client = MockClient((_) => throw Exception(''));
+        expect(_gradesApi.addGrade('1', '1', 'A', 'Good'),
+            throwsA(isInstanceOf<Failure>()));
+      });
+    });
+
+    group('updateGrade -', () {
+      test('When called & http client returns success response', () async {
+        ApiUtils.client = MockClient(
+            (_) => Future.value(Response(jsonEncode(mockGrade), 202)));
+        var _gradesApi = HttpGradesApi();
+
+        expect((await _gradesApi.updateGrade('1', 'A', 'Good')).toString(),
+            _grade.toString());
+      });
+
+      test('When called & http client throws Exceptions', () async {
+        var _gradesApi = HttpGradesApi();
+
+        ApiUtils.client = MockClient((_) => throw ForbiddenException(''));
+        expect(_gradesApi.updateGrade('1', 'A', 'Good'),
+            throwsA(isInstanceOf<Failure>()));
+
+        ApiUtils.client = MockClient((_) => throw NotFoundException(''));
+        expect(_gradesApi.updateGrade('1', 'A', 'Good'),
+            throwsA(isInstanceOf<Failure>()));
+
+        ApiUtils.client =
+            MockClient((_) => throw UnprocessableIdentityException(''));
+        expect(_gradesApi.updateGrade('1', 'A', 'Good'),
+            throwsA(isInstanceOf<Failure>()));
+
+        ApiUtils.client = MockClient((_) => throw Exception(''));
+        expect(_gradesApi.updateGrade('1', 'A', 'Good'),
+            throwsA(isInstanceOf<Failure>()));
+      });
+    });
+
+    group('deleteGrade -', () {
+      test('When called & http client returns success response', () async {
+        ApiUtils.client = MockClient((_) => Future.value(Response('', 204)));
+        var _gradesApi = HttpGradesApi();
+
+        expect(await _gradesApi.deleteGrade('1'), true);
+      });
+
+      test('When called & http client throws Exceptions', () async {
+        var _gradesApi = HttpGradesApi();
+
+        ApiUtils.client = MockClient((_) => throw ForbiddenException(''));
+        expect(_gradesApi.deleteGrade('1'), throwsA(isInstanceOf<Failure>()));
+
+        ApiUtils.client = MockClient((_) => throw NotFoundException(''));
+        expect(_gradesApi.deleteGrade('1'), throwsA(isInstanceOf<Failure>()));
+
+        ApiUtils.client = MockClient((_) => throw Exception(''));
+        expect(_gradesApi.deleteGrade('1'), throwsA(isInstanceOf<Failure>()));
+      });
+    });
+  });
+}
--- test/setup/test_helpers.dart
+++ test/setup/test_helpers.dart
@@ -1,15 +1,30 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_app/locator.dart';
+import 'package:mobile_app/services/API/assignments_api.dart';
 import 'package:mobile_app/services/API/contributors_api.dart';
+import 'package:mobile_app/services/API/grades_api.dart';
+import 'package:mobile_app/services/API/group_members_api.dart';
+import 'package:mobile_app/services/API/groups_api.dart';
 import 'package:mobile_app/services/API/users_api.dart';
 import 'package:mobile_app/services/local_storage_service.dart';
 import 'package:mockito/mockito.dart';
+
+Function deepEq = DeepCollectionEquality().equals;
 
 class NavigatorObserverMock extends Mock implements NavigatorObserver {}
 
 class LocalStorageServiceMock extends Mock implements LocalStorageService {}
 
 class ContributorsApiMock extends Mock implements ContributorsApi {}
+
+class GroupsApiMock extends Mock implements GroupsApi {}
+
+class GroupMembersApiMock extends Mock implements GroupMembersApi {}
+
+class AssignmentsApiMock extends Mock implements AssignmentsApi {}
+
+class GradesApiMock extends Mock implements GradesApi {}
 
 class UsersApiMock extends Mock implements UsersApi {}
 
@@ -29,6 +44,38 @@ ContributorsApi getAndRegisterContributorsApiMock() {
   return _contributorsApi;
 }
 
+GroupsApi getAndRegisterGroupsApiMock() {
+  _removeRegistrationIfExists<GroupsApi>();
+  var _groupsApi = GroupsApiMock();
+
+  locator.registerSingleton<GroupsApi>(_groupsApi);
+  return _groupsApi;
+}
+
+GroupMembersApi getAndRegisterGroupMembersApiMock() {
+  _removeRegistrationIfExists<GroupMembersApi>();
+  var _groupMembersApi = GroupMembersApiMock();
+
+  locator.registerSingleton<GroupMembersApi>(_groupMembersApi);
+  return _groupMembersApi;
+}
+
+AssignmentsApi getAndRegisterAssignmentsApiMock() {
+  _removeRegistrationIfExists<AssignmentsApi>();
+  var _assignmentsApi = AssignmentsApiMock();
+
+  locator.registerSingleton<AssignmentsApi>(_assignmentsApi);
+  return _assignmentsApi;
+}
+
+GradesApi getAndRegisterGradesApiMock() {
+  _removeRegistrationIfExists<GradesApi>();
+  var _gradesApi = GradesApiMock();
+
+  locator.registerSingleton<GradesApi>(_gradesApi);
+  return _gradesApi;
+}
+
 UsersApi getAndRegisterUsersApiMock() {
   _removeRegistrationIfExists<UsersApi>();
   var _usersApi = UsersApiMock();
@@ -40,12 +87,20 @@ UsersApi getAndRegisterUsersApiMock() {
 void registerServices() {
   getAndRegisterLocalStorageServiceMock();
   getAndRegisterContributorsApiMock();
+  getAndRegisterGroupsApiMock();
+  getAndRegisterGroupMembersApiMock();
+  getAndRegisterAssignmentsApiMock();
+  getAndRegisterGradesApiMock();
   getAndRegisterUsersApiMock();
 }
 
 void unregisterServices() {
   locator.unregister<LocalStorageService>();
   locator.unregister<ContributorsApi>();
+  locator.unregister<GroupsApi>();
+  locator.unregister<GroupMembersApi>();
+  locator.unregister<AssignmentsApi>();
+  locator.unregister<GradesApi>();
   locator.unregister<UsersApi>();
 }
 
