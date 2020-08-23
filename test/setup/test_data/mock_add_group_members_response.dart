f6132285cd617b9cc9920ae7d04680e1ac87df25
Nitish Aggarwal <royalnitish21@gmail.com>
2020-08-23 13:15:50 +0530

    test: Add mock data

---
+++ test/setup/test_data/mock_add_group_members_response.dart
@@ -0,0 +1,5 @@
+Map<String, dynamic> mockAddGroupMembersResponse = {
+  'added': ['test@test.com'],
+  'pending': ['pending@test.com'],
+  'invalid': ['invalid@test.com']
+};
---
+++ test/setup/test_data/mock_group_members.dart
@@ -0,0 +1,36 @@
+Map<String, dynamic> mockGroupMembers = {
+  'data': [
+    {
+      'id': '1',
+      'type': 'group_member',
+      'attributes': {
+        'group_id': 1,
+        'user_id': 1,
+        'created_at': '2020-08-15T14:36:38.228Z',
+        'updated_at': '2020-08-15T14:36:38.228Z',
+        'name': 'Test User',
+        'email': 'test@test.com'
+      },
+    }
+  ],
+  'links': {
+    'self': '{url}?page[number]=1',
+    'first': '{url}?page[number]=1',
+    'prev': null,
+    'next': '{url}?page[number]=2',
+    'last': '{url}?page[number]=1'
+  }
+};
+
+Map<String, dynamic> mockGroupMember = {
+  'id': '1',
+  'type': 'group_member',
+  'attributes': {
+    'group_id': 1,
+    'user_id': 1,
+    'created_at': '2020-08-15T14:36:38.228Z',
+    'updated_at': '2020-08-15T14:36:38.228Z',
+    'name': 'Test User',
+    'email': 'test@test.com'
+  }
+};
