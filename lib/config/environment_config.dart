2df4ea5f4e79457629a3b714a92d3880269f5f70
Nitish Aggarwal <royalnitish21@gmail.com>
2020-08-26 01:58:48 +0530

    Add Release Configuration (#12)

--- lib/config/environment_config.dart
+++ lib/config/environment_config.dart
@@ -3,4 +3,16 @@ class EnvironmentConfig {
     'CV_API_BASE_URL',
     defaultValue: 'https://circuitverse.org/api/v1',
   );
+
+  // GITHUB OAUTH ENV VARIABLES
+  static const String GITHUB_OAUTH_CLIENT_ID = String.fromEnvironment(
+    'GITHUB_OAUTH_CLIENT_ID',
+  );
+  static const String GITHUB_OAUTH_CLIENT_SECRET = String.fromEnvironment(
+    'GITHUB_OAUTH_CLIENT_SECRET',
+  );
+  static const String GITHUB_OAUTH_REDIRECT_URI = String.fromEnvironment(
+    'GITHUB_OAUTH_REDIRECT_URI',
+    defaultValue: 'circuitverse://auth/callback/github',
+  );
 }
