c2b7d79ecdd940db12240a4ebccefb05a51272b7
Manjot Sidhu <manjot.techie@gmail.com>
2020-11-16 17:01:55 +0530

    Add modal test for Links model.
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>

---
+++ test/setup/test_data/mock_links.dart
@@ -0,0 +1,7 @@
+Map<String, dynamic> mockLinks = {
+  'self': '{url}?page[number]=2',
+  'first': '{url}?page[number]=1',
+  'prev': '{url}?page[number]=1',
+  'next': '{url}?page[number]=3',
+  'last': '{url}?page[number]=3'
+};
