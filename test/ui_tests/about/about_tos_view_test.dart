c1f068e3692aeb72f52d072e38694fcb06a72031
Manjot Sidhu <manjot.gni@gmail.com>
2020-11-20 17:02:57 +0530

    About CV: Bringup Terms of Service Page. (#22)
    * About CV: Bringup Terms of Service Page.
    
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>
    
    * Add UI tests for tos page.
    
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>

---
+++ test/ui_tests/about/about_tos_view_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:mobile_app/locator.dart';
+import 'package:mobile_app/ui/views/about/about_tos_view.dart';
+import 'package:mobile_app/utils/router.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../setup/test_helpers.dart';
+
+void main() {
+  group('AboutTosViewTest -', () {
+    NavigatorObserver mockObserver;
+
+    setUpAll(() async {
+      SharedPreferences.setMockInitialValues({});
+      await setupLocator();
+    });
+
+    setUp(() => mockObserver = NavigatorObserverMock());
+
+    Future<void> _pumpAboutTosView(WidgetTester tester) async {
+      await tester.pumpWidget(
+        GetMaterialApp(
+          onGenerateRoute: CVRouter.generateRoute,
+          navigatorObservers: [mockObserver],
+          home: AboutTosView(),
+        ),
+      );
+
+      /// The tester.pumpWidget() call above just built our app widget
+      /// and triggered the pushObserver method on the mockObserver once.
+      verify(mockObserver.didPush(any, any));
+    }
+
+    testWidgets('finds RichText and Text Widgets on AboutTosView',
+        (WidgetTester tester) async {
+      await _pumpAboutTosView(tester);
+
+      expect(find.byType(RichText), findsWidgets);
+      expect(find.byType(Text), findsWidgets);
+    });
+  });
+}
