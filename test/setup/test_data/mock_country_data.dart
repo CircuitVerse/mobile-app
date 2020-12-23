62554d05f9d2b0dd62ff654c537a13ad076ed179
Muppuri Siva Jagadesh <56889104+jaggu21@users.noreply.github.com>
2020-12-23 08:59:12 +0530

    Added Typeahead field for Country and Educational Institute fields (#43)
    * Added typeahead field
    
    * added typeahead field
    
    * Added API tests
    
    * Separated Country and Institute Functions
    
    * Moved Constants
    
    * Formatted Test

---
+++ test/setup/test_data/mock_country_data.dart
@@ -0,0 +1,62 @@
+List mockCountries = [
+  {
+    'name': 'Test',
+    'topLevelDomain': ['.Test'],
+    'alpha2Code': 'Test',
+    'alpha3Code': 'Test',
+    'callingCodes': ['Test'],
+    'capital': 'Test',
+    'altSpellings': ['Test', 'Test'],
+    'region': 'Test',
+    'subregion': 'Test',
+    'population': 0,
+    'latlng': [0, 0],
+    'demonym': 'Test',
+    'area': 0,
+    'gini': 0,
+    'timezones': ['Test'],
+    'borders': ['Test'],
+    'nativeName': 'Test',
+    'numericCode': 'Test',
+    'currencies': [
+      {'code': 'Test', 'name': 'Test', 'symbol': 'Test'}
+    ],
+    'languages': [
+      {
+        'iso639_1': 'Test',
+        'iso639_2': 'Test',
+        'name': 'Test',
+        'nativeName': 'Test'
+      },
+      {
+        'iso639_1': 'Test',
+        'iso639_2': 'Test',
+        'name': 'Test',
+        'nativeName': 'Test'
+      },
+      {
+        'iso639_1': 'Test',
+        'iso639_2': 'Test',
+        'name': 'Test',
+        'nativeName': 'Test'
+      }
+    ],
+    'translations': {
+      'de': 'Test',
+      'es': 'Test',
+      'fr': 'Test',
+      'ja': 'Test',
+      'it': 'Test',
+      'br': 'Test',
+      'pt': 'Test',
+      'nl': 'Test',
+      'hr': 'Test',
+      'fa': 'Test'
+    },
+    'flag': 'Test',
+    'regionalBlocs': [
+      {'acronym': 'Test', 'name': 'Test', 'otherAcronyms': [], 'otherNames': []}
+    ],
+    'cioc': 'Test'
+  }
+];
