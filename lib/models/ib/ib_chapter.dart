565337d766437e5e8ea689575984793d6de1f7ed
Manjot Sidhu <manjot.techie@gmail.com>
2021-06-13 19:20:59 +0530

    Models: Replace IbRawPage with IbChapter.
    * In-order to build Navigation and Drawer List Items, we realized that IbRawPage is just too overkill for the
    requirement. We just need title and ids to build Navigation and Drawer Lists. Also, since data is not getting
    serialized directly from API, IbEngineService is expected to create models from this RAW JSON. Moreover, this
    IbRawPage is already included in IbRawPageData which will fetched for every page content.
    
    * From now onwards, IbChapter[] will be received from the IbEngineService and will be used just for Book
    Structure and Navigation.
    
    * IbChapter includes previous page id, next page id because IbRawPageData won't have this. The only way to get
    prev and next is finding them while preparing Navigation and structure. Accordingly, IbPageView should also
    require prev and next from the IbLandingView to show Previous and Next buttons action.
    
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>

---
+++ lib/models/ib/ib_chapter.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class IbChapter {
+  final String id;
+  final String value;
+  final String prev;
+  final String next;
+  final List<IbChapter> items;
+
+  IbChapter(
+      {@required this.id,
+      @required this.value,
+      this.prev,
+      this.next,
+      this.items});
+}
