f404836b2a85a2831dcacea64f62bfa24e514d8d
pavanjoshi914 <pavanj914@gmail.com>
2021-08-07 07:30:47 +0530

    fix(i18n): remove unused import for fluter_localizations signed-off-by: Pavan Joshi <pavanj914@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get/get.dart';
 import 'package:mobile_app/cv_theme.dart';
