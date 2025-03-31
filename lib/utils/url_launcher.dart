6e7aeae3af52ffc0b9dfd2e74a0ec7fea01b4a06
NITIN <nitinsohal@NITINs-MacBook-Air.local>
2025-04-01 01:49:08 +0530

    changes

--- lib/utils/url_launcher.dart
+++ lib/utils/url_launcher.dart
@@ -1,9 +1,34 @@
 import 'package:url_launcher/url_launcher_string.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 
-void launchURL(String url) async {
-  if (await canLaunchUrlString(url)) {
-    await launchUrlString(url);
+void launchURL(BuildContext context,String url) async {
+  if (url.startsWith('mailto:')) {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(
+        uri,
+        mode: LaunchMode.externalApplication,
+      );
+    } else {
+      final email = url.replaceFirst('mailto:', '');
+      Clipboard.setData(ClipboardData(text: email));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Email copied to clipboard: $email"),
+          duration: Duration(seconds: 1),
+        ),
+      );
+      await Future.delayed(Duration(seconds: 1));
+      await launchUrlString(
+        'https://mail.google.com/mail/u/0/?fs=1&to=$email&tf=cm',
+        mode: LaunchMode.externalApplication,
+      );
+    }
   } else {
-    throw 'Could not launch $url';
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } 
   }
 }
