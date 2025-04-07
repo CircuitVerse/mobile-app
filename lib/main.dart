d5ff663b264642bbef0be1be14dafda770ee1158
NITIN <nitinsohal@NITINs-MacBook-Air.local>
2025-04-07 14:46:27 +0530

    feat(language): enhance language switcher UI and add expandable language list with dynamic support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
--- lib/ui/components/cv_drawer.dart
+++ lib/ui/components/cv_drawer.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
