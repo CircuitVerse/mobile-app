aa3e4f6b5c21bace22656eb5e559812f5885647e
SantamRC <santamdev404@gmail.com>
2026-03-08 23:37:42 +0530

    remove json extension from api endpoints for jekyll-admin compatibility

--- lib/services/API/ib_api.dart
+++ lib/services/API/ib_api.dart
@@ -19,8 +19,8 @@ class HttpIbApi implements IbApi {
   Future<List<Map<String, dynamic>>>? fetchApiPage({String id = ''}) async {
     var _url =
         id == ''
-            ? '${EnvironmentConfig.IB_API_BASE_URL}.json'
-            : '${EnvironmentConfig.IB_API_BASE_URL}/$id.json';
+            ? EnvironmentConfig.IB_API_BASE_URL
+            : '${EnvironmentConfig.IB_API_BASE_URL}/$id';
 
     try {
       if (await _db.isExpired(_url)) {
